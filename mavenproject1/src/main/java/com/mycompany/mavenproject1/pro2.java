3d187f36ca8fdf2f867c547e4565060de34fd8bb
Pc <Pc@DESKTOP-01BU0DD>
2025-09-25 21:13:03 +0800

    array

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/pro2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Pc
+ */
+import java.util.HashSet;
+public class pro2 {
+    public static void main(String[] args) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");  // Duplicate
+    cars.add("Mazda");
+    System.out.println(cars);
+    }
+}
